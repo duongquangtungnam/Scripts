f6ee3c34aaf3b86e09abc4c37c78bf0b861e3620
duongquangtungnam <tungnam280413@gmail.com>
2026-06-25 19:48:39 +0700

    Update TungNamXRedz.lua

--- TungNamXRedz.lua
+++ TungNamXRedz.lua
@@ -1,1 +1,7 @@
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/duongquangtungnam/Scripts/refs/heads/main/NotifyBloxFruit.lua"))()
+    end)
+end)
+
 loadstring(game:HttpGet"https://pastefy.app/MsZyLO5w/raw")()
