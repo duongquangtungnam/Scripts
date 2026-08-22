5fb0a03c5ff0fedcb230ebce37cbfa2cd88bfdde
tnam.dev <tungnam280413@gmail.com>
2026-08-22 23:08:57 +0700

    Update TungNamXRedz.lua

--- TungNamXRedz.lua
+++ TungNamXRedz.lua
@@ -4,4 +4,4 @@ task.spawn(function()
     end)
 end)
 
-loadstring(game:HttpGet"https://pastefy.app/MsZyLO5w/raw")()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4e2312362ff42cc3a2f681766de2d0fa96ccffab7740b521ff033ae72de9ab20/download"))()
